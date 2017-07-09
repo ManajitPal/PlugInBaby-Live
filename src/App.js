b19665861ada7bffa541680fe6c9da4711930d0f
ManajitPal <manajitpal95@gmail.com>
2017-07-10 01:20:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+import Home from './components/Home/Home';
+import Signup from './components/Signup/Signup';
+import Login from './components/Login/Login';
+import Artist from './components/Artist/Artist';
+import Employer from './components/Employer/Employer';
+import Fan from './components/Fan/Fan';
+import { Route } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Route exact={true} path="/" Component={Home} />
+        <Route path="/login" Component={Login} />
+        <Route path="/signup" Component={Signup} />
+        <Route path="/fan" Component={Fan} />
+        <Route path="/employer" Component={Employer} />
+        <Route path="/artist" Component={Artist} />
+      </div>
+    );
+  }
+}
+
+export default App;
